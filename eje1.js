e5df14b32410ec65974e3a381db7cccd365a9be2
Nadia Ruiz de Chávez <68034877+nadia19081d@users.noreply.github.com>
2023-07-10 17:28:31 -0600

    Exercise 11 resolved

--- eje1.js
+++ eje1.js
@@ -158,7 +158,7 @@ Course: 2 meses
 Carrera 6 meses
 Master: 12 meses */ 
 function cursoMensualidad(curso,descuento) {
-  var costo = 50;  
+  var costo = 0;  
   var duraciones; 
 switch (curso) {
   case "course":
@@ -188,3 +188,34 @@ console.log("Total costo: " + costo + " duración del curso "+ duraciones);
 var curso = prompt("¿Qué curso estas tomando? ")
 var descuento = prompt("¿Tienes beca?, si si especificala aqui")
 console.log(cursoMensualidad(curso, descuento));
+
+/* 11. Realizar un programa que ayude a calcular el total a pagar de acuerdo a la distancia recorrida por un vehículo con cargo extra por los litros consumidos, tomando en consideración lo siguiente:
+Si el vehículo es “coche”, el precio kilometro ha de ser 0.20, si es “moto” ha de ser 0.10 y si es “autobús” 0.5.
+Si los litros consumidos están entre 0 y 100 se ha de añadir 5 al costo total, si es mayor la cantidad de litros consumidos se ha de añadir 10 al total. Considere qué:
+total a pagar = (precio kilometro x kms recorridos) + extra por litros consumidos. */
+function totalPagar(precioKm,kmRecorridos) {
+  var precioKm = 0;  
+  var extraKmRecorridos;
+  var total;  
+switch (precioKm) {
+  case "coche":
+    precioKm = 0.2;
+    break;
+  case "moto":
+    precioKm = 0.10 ;
+    break;
+  case "autobús":
+    precioKm = 0.5 ;
+    break;  
+}
+if (kmRecorridos >0 && kmRecorridos <100){
+  extraKmRecorridos = 5; 
+} else if (kmRecorridos >100){
+  extraKmRecorridos = 10; 
+}
+total = (precioKm * kmRecorridos) + extraKmRecorridos;
+return total;
+}
+var precioKm = Number(prompt("¿En que vehiculo viajaste? "))
+var kmRecorridos = Number(prompt("¿Cuatos km recorriste?"))
+console.log(totalPagar(precioKm, kmRecorridos));
