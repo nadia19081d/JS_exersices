25df3c48ab087781ad30af5b3c8788b9f51708dd
Nadia Ruiz de Chávez <68034877+nadia19081d@users.noreply.github.com>
2023-07-11 12:07:21 -0600

    Excercise 2 resolved

--- eje3.js
+++ eje3.js
@@ -9,6 +9,24 @@ while (i <= num) {
     i++
 }
 // 2. Crea un programa que solicite al usuario 2 números entre 1 y 50. Posteriormente mostrar en consola los números del 1 hasta el 50, pero añadir el mensaje “¡Lotería!” solo al mostrar los números indicados por el usuario.
+
+var num = Number(prompt("Ingresa un número entre 1 y 50"))
+var num2 = Number(prompt("Ingresa un número entre 1 y 50"))
+function loteria(num,num2){
+    var i = 1; 
+    while (i < 50) { 
+        console.log(i)
+        i++
+        if(i== num || i== num2){
+            console.log(i + " Loteria") 
+
+        }
+    }
+}
+console.log(loteria(num,num2))
+
 // 3. Crea un programa que solicite al usuario números, si lo que este introduce es un número guardarlo en un arreglo. Para terminar el capturar el usuario debe ingresar el número 0. Finalmente mostrar la lista de números capturados en pantalla o en la consola.
+
+
 // 4. Crea un programa que solicite al usuario letras o palabras, si es así guardar el resultado. Para terminar de capturar el usuario no debe escribir valor alguno. Al terminar de capturar, mostrar en pantalla la concatenación de todas las palabras capturadas.
 // 5. Crea un programa que solicite al usuario un día de la semana (ej: lunes, jueves, domingo, etc). El programa mostrará un mensaje personalizado para cada día de la semana por medio de un alert. Y seguirá pidiendo al usuario introducir otro día. En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
